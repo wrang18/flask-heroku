8e8e57c89b2a8795244fca86c3e8e29b08077930
wrang18 <89901980+wrang18@users.noreply.github.com>
2021-09-01 14:50:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, jsonify
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import cv2
+
+camera = cv2.VudeoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')
 
 app = Flask(__name__)
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show resul
 
 data = [
         {
@@ -29,5 +43,20 @@ def hello():
 def get_api():
     return jsonify(data)
 
+@app.route('/hello/<string:name>')
+def Home(name):
+    return render_template('home.html', name_html=name)
+
+@app.route('/name')
+    return"<font color=blue>วรางคณา นุชเกษม</font> <br> เลขที่ 18 ชั้น ม.4/10"
+
+@app.route('/video')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
